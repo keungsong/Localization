1122295c00ce81f7854fd31ec23ef0e07779acea
KeungXiongV <74707079+KeungXiongV@users.noreply.github.com>
2022-06-02 16:12:23 +0700

    new commit

--- lib/LocaString.dart
+++ lib/LocaString.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 
 class LocalString extends Translations {
   @override
-  // TODO: implement keys
   Map<String, Map<String, String>> get keys => {
         'en_US': {
           'title': 'Language Translate',
